862deca10a50c6cbbef7c6ba396b03b67d96e819
Renato Castro <renatocastro02@hotmail.com>
2021-05-15 16:55:52 -0300

    feat: add validation to both cpf and phone number

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import * as Yup from 'yup';
+import { validateCPF, validatePhone } from 'validations-br';
 import { UserRepository } from '../repositories';
 import { AppError } from '../errors/AppError';
 
@@ -18,8 +19,14 @@ class UserController {
     const schema = Yup.object().shape({
       firstname: Yup.string().required('Nome obrigatório'),
       lastname: Yup.string().required('Sobreneme obrigatório'),
-      cpf: Yup.string().required('CPF obrigatório'),
-      phone: Yup.string().required('Telefone obrigatório'),
+      cpf: Yup.string()
+        .required('CPF obrigatório')
+        .test('validação do cpf', 'CPF Inválido', value => validateCPF(value)),
+      phone: Yup.string()
+        .required('Telefone obrigatório')
+        .test('validação do telefone', 'Telefone Inválido', value =>
+          validatePhone(value),
+        ),
     });
 
     try {
