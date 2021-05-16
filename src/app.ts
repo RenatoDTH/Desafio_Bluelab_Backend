a241ec8d7093579ca1896e18db7087b28adc002b
Renato Castro <renatocastro02@hotmail.com>
2021-05-15 22:47:35 -0300

    test: ensure statusCode 201 returns on succed creation

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import { routes } from './routes';
+import 'reflect-metadata';
+import { AppError } from './errors/AppError';
+import createConnection from './database/connection';
+
+createConnection();
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'Error',
+    message: `Internal server error ${err.message}`,
+  });
+});
+
+export { app };
