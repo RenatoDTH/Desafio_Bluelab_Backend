8fe94ec9c9ec1dffe4e2d1dbb4841fda2c756850
Renato Castro <renatocastro02@hotmail.com>
2021-05-16 14:09:59 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import { routes } from './routes';
 import 'reflect-metadata';
 import { AppError } from './errors/AppError';
@@ -7,6 +8,7 @@ import createConnection from './database/connection';
 
 createConnection();
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
